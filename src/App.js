6b97cf43cb4e2d13fa9b0377b023e794db2ed7a3
LucaPizzagalli <lucapizzagalli@gmail.com>
2021-11-14 17:04:22 +0100

    github test

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <ThemeProvider theme={responsiveFontSizes(createMuiTheme(
       {
-        palette: { ...basePalette, type: themeType },
+        palette: { ...basePalette, type: themeType},
         spacing: themeSpacing,
       }
     ))}>
